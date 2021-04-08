631634087754ea85aaeed49598d33d604dac6f22
oscarcortez <oscarkortez@gmail.com>
2021-04-08 19:35:55 -0400

    Add project files.

---
+++ CSharp.Solid.InterfaceSegregation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharp.Solid.InterfaceSegregation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
