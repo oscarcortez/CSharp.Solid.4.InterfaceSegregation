6706557c96ef5f81cdbbc4f226f2fac10f67b6e2
oscarcortez <oscarkortez@gmail.com>
2021-04-08 19:38:18 -0400

    interface IProduct was created only to be implemented with Laptop Product

---
+++ CSharp.Solid.InterfaceSegregation.Models/IProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Solid.InterfaceSegregation.Models
+{
+    interface IProduct
+    {
+        decimal Price { get; set; }
+        int Stock { get; set; }
+        double InchesScreen { get; set; }
+        double Weight { get; set; }
+        string OperatingSystem { get; set; }
+        string Color { get; set; }
+        int Ram { get; set; }
+        string Processor { get; set; }
+    }
+}
---
+++ CSharp.Solid.InterfaceSegregation.Models/Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Solid.InterfaceSegregation.Models
+{
+    public class Laptop : IProduct
+    {
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Color { get; set; }
+        public double InchesScreen { get; set; }
+        public double Weight { get; set; }
+        public string OperatingSystem { get; set; }
+        public int Ram { get; set; }
+        public string Processor { get; set; }
+    }
+}
